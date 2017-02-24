b579c5b5ebfced735e604d4243d88f9f964e9655
Mathieu Roblin <roblin.mathieu@gmail.com>
2017-02-24 10:43:32 +0800

    ADD redux store to the boilerplate

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, compose } from 'react-redux'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { browserHistory } from 'react-router'
+
+// import rootReducer from './reducers/rootReducer'
+
+const defaultState = {}
+
+// export default const store = createStore(rootReducer, defaultState)
+
+// export const history = syncHistoryWithStore(browserHistory, store)
