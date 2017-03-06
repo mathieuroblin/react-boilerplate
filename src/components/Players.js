dc9ee45124bde0563342b08b3ffe309ce3b322e1
Mathieu Roblin <roblin.mathieu@gmail.com>
2017-03-06 10:51:21 +0800

    The player input is now a controlled element

--- src/components/Players.js
+++ src/components/Players.js
@@ -2,24 +2,28 @@ import React from 'react'
 
 import { addPlayer, editInput } from "../actions/actionCreators"
 
-const test = (players) => {
-  if (players) {
-    return players.map( (player) => (<div>Test</div>) )
+const displayPlayersList = (players_list) => {
+  if (players_list) {
+    return players_list.map( (player) => (<div>{ player }</div>) )
   }
 }
 
 const Players = (props) => {
+  const player_name = props.state.getIn(["ui", "input-player"], "")
+  const players_list = props.state.getIn(["data", "players"], [])
+
   return (
     <div>
       <h1>Players</h1>
-      {/* <div>{ test(props.state) }</div> */}
+      { displayPlayersList(players_list) }
       <div>
         <input id="input-name"
                onChange={ (event) => {
                  props.dispatch(editInput(["ui", "input-player"], event.target.value))
-               }}/>
+               }}
+               value={ player_name }/>
         <button onClick={ (event) => {
-                  props.dispatch(addPlayer(props.state.getIn(["ui", "input-player"])))
+                  props.dispatch(addPlayer(player_name))
                 }}>
                 Add a player
         </button>
--- src/reducers/stateReducer.js
+++ src/reducers/stateReducer.js
@@ -7,12 +7,15 @@ import {
 
 const state = (state = Map(), action) => {
   switch (action.type) {
+
     case ACTION_ADD_PLAYER:
       const players = state.getIn(["data", "players"], List())
 
       return state.setIn(["data", "players"], players.push(action.name))
+
     case ACTION_EDIT_INPUT:
       return state.setIn(action.path, action.value)
+
     default:
       return state
   }
