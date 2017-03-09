d4c34acb3857061e3f3a0dad2bb5bf015c33e407
Mathieu Roblin <roblin.mathieu@gmail.com>
2017-03-09 16:27:58 +0800

    ADD local state for components

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { Map } from 'immutable'
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 
-import store from '../store'
 import StateToProps from './StateToProps'
+import rootReducer from '../reducers/rootReducer'
 
 import Players from '../components/Players'
 
-
+// CAN't MAKE IT WORK WITH A FUNCTION TO GENERATE THE STORE
+const tempStore = createStore(rootReducer, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const tempStore2 = createStore(rootReducer, {state: Map({ui: Map({'input-player': 'init'})})}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 storiesOf('My components', module)
-  .addDecorator((story) => (<Provider store={ store }>
-                              <StateToProps>
-                                { story() }
-                              </StateToProps>
-                            </Provider>
+  .add('Players no init', () => (
+    <Provider store={ tempStore }>
+      <StateToProps>
+        <Players />
+      </StateToProps>
+    </Provider>
   ))
-
-  .add('Players', () => (
-    <Players />
+  .add('Players for test', () => (
+    <Provider store={ tempStore2 }>
+      <StateToProps>
+        <Players />
+      </StateToProps>
+    </Provider>
   ))
